d51a3993c8c9e7c2328c70c1161304c87075e15a
zjregee <zjregee@gmail.com>
2024-09-05 20:16:15 +0800

    update

--- run_all_benchmark.sh
+++ run_all_benchmark.sh
@@ -1,5 +1,2 @@
-rm -rf /home/zjregee/Code/diskv/test
-./build/benchmark/leveldb_benchmark 1000000 32 172 /home/zjregee/Code/diskv/test
-
 ./make_disk.sh test_disk
 ./build/benchmark/diskv_hashtable_benchmark 1000000 test_disk
